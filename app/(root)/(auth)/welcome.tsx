92a0cfc191316562ef154f07d8216a442d04a334
melisguclu <melisguclu26@gmail.com>
2025-01-11 18:53:00 +0300

    implement onboarding screens with swiper

--- app/(root)/(auth)/welcome.tsx
+++ app/(root)/(auth)/welcome.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Text, Touchable, TouchableOpacity} from  'react-native';
+import { Text, View, Image,  TouchableOpacity} from  'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Swiper from 'react-native-swiper';
+import { useState } from 'react';
+import { onboarding } from '@/constants';
+import { router } from 'expo-router';
+import CustomButton from '@/components/customButton';
+
 
 const Onboarding = () => {
 
   const swiperRef = React.useRef(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
 
 
   return(
-    <SafeAreaView className='flex h-full items-center justify-between bg-white'>
+    <SafeAreaView edges={['bottom']} className='flex h-full items-center justify-between bg-white'>
       <TouchableOpacity
         onPress={() => {
-          console.log('Skip');
+          router.replace('/(root)/(auth)/sign-up');
         }
       }
       className= 'w-full flex justify-end items-end p-5'>
@@ -22,17 +29,28 @@ const Onboarding = () => {
       <Swiper
         ref={swiperRef}
         loop={false}
-        showsPagination={false}
-        showsButtons={false}
-        index={0}
-        onIndexChanged={(index) => {
-          console.log('Index:', index);
-        }}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
       >
-        <Text className='text-black text-2xl font-JakartaBold'>Welcome to the App</Text>
-        <Text className='text-black text-2xl font-JakartaBold'>Welcome to the App</Text>
-        <Text className='text-black text-2xl font-JakartaBold'>Welcome to the App</Text>
+        {
+          onboarding.map((item) => (
+            <View key={item.id} className='flex p-5 items-center justify-center'>
+              <Image source={item.image} className='w-full h-[300px]' resizeMode='contain' />
+              <View className="flex flex-row items-center justify-center w-full mt-10" >
+                <Text className='text-black text-3xl font-bold mx-10 text-center'>{item.title}</Text>
+              </View>
+              <Text className='text-md font-JakartaSemiBold mx-10 mt-3 text-center text-[#858585]'>{item.description}</Text>
+            </View>
+          ))
+        }
       </Swiper>
+
+      <CustomButton title={isLastSlide ? "Get Started" : "Next"} onPress={() => isLastSlide ? router.replace('/(root)/(auth)/sign-up') : swiperRef.current?.scrollBy(1)} className='w-11/12 mt-10'  />
 
     </SafeAreaView>
 
